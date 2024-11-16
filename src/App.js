2be39b137ecd9e55ef45a566ae473fc181f227a9
py chua <chua86614@gmail.com>
2024-11-16 16:08:15 +0800

    Multiple components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
-  // can use for numbers, strings and arrays but cannot use for booleans and objects
-  const link = "https://www.google.com"
-
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{ title }</h1>
-        <p>Liked { likes } times</p>
-
-        <a href={ link }>Google Site</a>
+        <Home />
       </div>
     </div>
   );
